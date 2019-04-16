73b4f1cb3e1ecf781919194aee01758aecab13ef
kirbycampbell <jkirbycampbell@gmail.com>
2019-04-15 22:02:47 -0700

    Updated CSS

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="App">
         <div className="border" />
         <div className="name-bnr">
           <h1>Kirby Campbell</h1>
