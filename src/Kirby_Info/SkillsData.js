4b96a25dcf255d30b8ed98d8f00fe1441ada1cee
kirbycampbell <jkirbycampbell@gmail.com>
2019-04-19 17:18:03 -0700

    Adding Skills

--- src/Kirby_Info/SkillsData.js
+++ src/Kirby_Info/SkillsData.js
@@ -27,3 +27,40 @@ export const programs = [
   "Maya",
   "After Effects"
 ];
+
+export const projects = {
+  portfolio: {
+    1: {
+      name: "Medium Creator (Blog Clone)",
+      framework: "React",
+      backend: "FireStore",
+      hosted: "Heroku"
+    },
+    2: {
+      name: "WhatsApp (Chat App Clone)",
+      framework: "React Hooks",
+      backend: "GraphQl & Amplify",
+      hosted: "AWS"
+    },
+    3: {
+      name: "Beverage Manager",
+      framework: "React & Redux",
+      backend: "Ruby on Rails",
+      hosted: "Heroku & AWS"
+    },
+    4: {
+      name: "News Journal",
+      framework: "Vue",
+      backend: "None",
+      hosted: "Not Yet"
+    },
+    5: {
+      name: "Meditation Timer",
+      framework: "React Hooks",
+      backend: "None yet",
+      hosted: "Not yet"
+    }
+  },
+  music: ["Produced ~ 30 songs over last 5 years."],
+  video: ["Editor & Cinematographer of Virtual Reality Music Video"]
+};
