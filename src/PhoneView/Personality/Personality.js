78fd8fa49d7ac7ae9ce6b2ef76711abbec2a4c3f
kirbycampbell <jkirbycampbell@gmail.com>
2019-04-20 10:25:58 -0700

    Adding Personality Section

---
+++ src/PhoneView/Personality/Personality.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Personality.css";
+
+const Personality = () => {
+  return <div>heyoo personality</div>;
+};
+
+export default Personality;
--- src/PhoneView/PhoneView.js
+++ src/PhoneView/PhoneView.js
@@ -5,11 +5,13 @@ import SkillList from "./SkillList";
 import ProjectContainer from "./ProjectContainer";
 import NavBar from "./NavBar";
 import Education from "./Education";
+import Personality from "./Personality/Personality";
 
 const PhoneView = () => {
   const [projectShow, setProjectShow] = useState(false);
   const [skillShow, setSkillShow] = useState(false);
   const [educationShow, setEducationShow] = useState(false);
+  const [personalityShow, setPersonalityShow] = useState(true);
 
   const handleSkillShow = () => {
     setSkillShow(!skillShow);
@@ -22,6 +24,10 @@ const PhoneView = () => {
   const handleEducationShow = () => {
     setEducationShow(!educationShow);
   };
+
+  const handlePersonalityShow = () => {
+    setPersonalityShow(!personalityShow);
+  };
   return (
     <div className="App">
       <div className="border" />
@@ -29,7 +35,7 @@ const PhoneView = () => {
       <Contacts />
       {/*::::::::: SKILL SECTION ::::::::: */}
       <div className="divider" />
-      <div className="skills-title" onClick={handleSkillShow}>
+      <div className="titles" onClick={handleSkillShow}>
         Skills
       </div>
       <div className="divider" />
@@ -38,7 +44,7 @@ const PhoneView = () => {
       </div>
       {/*::::::::: PROJECT SECTION ::::::::: */}
       <div className="divider" />
-      <div className="project-title" onClick={handleProjectShow}>
+      <div className="titles" onClick={handleProjectShow}>
         Projects
       </div>
       <div className="divider" />
@@ -46,12 +52,19 @@ const PhoneView = () => {
       {!projectShow && <div className="skill-container" />}
       {/*:::::::::: EDUCATION SECTION :::::::::: */}
       <div className="divider" />
-      <div className="education-title" onClick={handleEducationShow}>
+      <div className="titles" onClick={handleEducationShow}>
         Education
       </div>
       <div className="divider" />
       {educationShow && <Education />}
       {!educationShow && <div className="skill-container" />}
+      {/*:::::::::: PERSONALITY SECTION :::::::::: */}
+      <div className="divider" />
+      <div className="titles" onClick={handlePersonalityShow}>
+        Personality
+      </div>
+      <div className="divider" />
+      {personalityShow && <Personality />}
     </div>
   );
 };
