df36718ef670fcf8f1222588b032bd848d0a362d
kirbycampbell <jkirbycampbell@gmail.com>
2019-04-21 10:08:50 -0700

    adding

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "core-js/fn/number/is-nan";
+import "core-js/es7/";
+import "core-js/es6/";
+import "raf/polyfill";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
