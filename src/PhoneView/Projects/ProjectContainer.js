d2f725884ef4b6e9b2ec336b07556f47660ce886
kirbycampbell <jkirbycampbell@gmail.com>
2019-05-04 23:16:12 -0700

    Animating Hover interactions

--- src/PhoneView/Projects/ProjectContainer.js
+++ src/PhoneView/Projects/ProjectContainer.js
@@ -3,68 +3,98 @@ import "./ProjectContainer.css";
 import { projects } from "../../Kirby_Info/SkillsData";
 
 const ProjectContainer = () => {
+  const [style, setFadeIn] = useState({
+    proj: "",
+    in: false,
+    style: "project-row-phone"
+  });
+  const projItem = projects.portfolio;
+
   const mouseEnter = name => {
-    console.log(name);
+    setFadeIn({ proj: name, in: true });
   };
   const mouseLeave = name => {
-    console.log(name);
+    setFadeIn({ proj: name, in: false });
   };
+
   return (
     <div>
       {" "}
       <div className="project-container">
         <div className="RowPhone">
           <div
-            className="project-row-phone"
-            onMouseEnter={() => mouseEnter(projects.portfolio.name1)}
-            onMouseLeave={() => mouseLeave(projects.portfolio.name1)}
+            className={
+              "project-row-phone " +
+              `${style.proj === projItem.name1 ? "hovered" : null}`
+            }
+            onMouseEnter={() => mouseEnter(projItem.name1)}
+            onMouseLeave={() => mouseLeave(projItem.name1)}
           >
-            {projects.portfolio.name1}
-            <br /> Framework: {projects.portfolio.framework1}
-            <br /> Backend: {projects.portfolio.backend1}
-            <br /> Hosted On: {projects.portfolio.hosted1}
+            {style.proj === projItem.name1 && style.in && (
+              <div>
+                <br /> Framework: {projItem.framework1}
+                <br /> Backend: {projItem.backend1}
+                <br /> Hosted On: {projItem.hosted1}{" "}
+              </div>
+            )}
+            {projItem.name1}
           </div>
 
           <div
-            className="project-row-phone"
-            onMouseEnter={() => mouseEnter(projects.portfolio.name4)}
-            onMouseLeave={() => mouseLeave(projects.portfolio.name4)}
+            className={
+              "project-row-phone " +
+              `${style.proj === projItem.name4 ? "hovered" : null}`
+            }
+            onMouseEnter={() => mouseEnter(projItem.name4)}
+            onMouseLeave={() => mouseLeave(projItem.name4)}
           >
-            {projects.portfolio.name4}
-            <br /> Framework: {projects.portfolio.framework4}
+            {projItem.name4}
+            <br /> Framework: {projItem.framework4}
           </div>
           <div
-            className="project-row-phone"
-            onMouseEnter={() => mouseEnter(projects.portfolio.name5)}
-            onMouseLeave={() => mouseLeave(projects.portfolio.name5)}
+            className={
+              "project-row-phone " +
+              `${style.proj === projItem.name5 ? "hovered" : null}`
+            }
+            onMouseEnter={() => mouseEnter(projItem.name5)}
+            onMouseLeave={() => mouseLeave(projItem.name5)}
           >
-            {projects.portfolio.name5}
-            <br /> Framework: {projects.portfolio.framework5}
+            {projItem.name5}
+            <br /> Framework: {projItem.framework5}
           </div>
         </div>
         <div className="RowPhone">
           <div
-            className="project-row-phone"
-            onMouseEnter={() => mouseEnter(projects.portfolio.name2)}
-            onMouseLeave={() => mouseLeave(projects.portfolio.name2)}
+            className={
+              "project-row-phone " +
+              `${style.proj === projItem.name2 ? "hovered" : null}`
+            }
+            onMouseEnter={() => mouseEnter(projItem.name2)}
+            onMouseLeave={() => mouseLeave(projItem.name2)}
           >
-            {projects.portfolio.name2}
-            <br /> Framework: {projects.portfolio.framework2}
-            <br /> Backend: {projects.portfolio.backend2}
-            <br /> Hosted On: {projects.portfolio.hosted2}
+            {projItem.name2}
+            <br /> Framework: {projItem.framework2}
+            <br /> Backend: {projItem.backend2}
+            <br /> Hosted On: {projItem.hosted2}
           </div>
           <div
-            className="project-row-phone"
-            onMouseEnter={() => mouseEnter(projects.portfolio.name3)}
-            onMouseLeave={() => mouseLeave(projects.portfolio.name3)}
+            className={
+              "project-row-phone " +
+              `${style.proj === projItem.name3 ? "hovered" : null}`
+            }
+            onMouseEnter={() => mouseEnter(projItem.name3)}
+            onMouseLeave={() => mouseLeave(projItem.name3)}
           >
-            {projects.portfolio.name3}
-            <br /> Framework: {projects.portfolio.framework3}
-            <br /> Backend: {projects.portfolio.backend3}
-            <br /> Hosted On: {projects.portfolio.hosted3}
+            {projItem.name3}
+            <br /> Framework: {projItem.framework3}
+            <br /> Backend: {projItem.backend3}
+            <br /> Hosted On: {projItem.hosted3}
           </div>
           <div
-            className="project-row-phone"
+            className={
+              "project-row-phone " +
+              `${style.proj === "Flatiron" ? "hovered" : null}`
+            }
             onMouseEnter={() => mouseEnter("Flatiron")}
             onMouseLeave={() => mouseLeave("Flatiron")}
           >
