d788ea9938db686bfa67d16d175101700fb82328
kirbycampbell <jkirbycampbell@gmail.com>
2019-05-04 22:31:14 -0700

    Adjusting Projects Area

--- src/DesktopView/Projects/ProjectContainerD.js
+++ src/DesktopView/Projects/ProjectContainerD.js
@@ -8,36 +8,36 @@ const ProjectContainerD = () => {
       {" "}
       <div className="project-containerD">
         <div className="Row1">
-          <div className="project-row-left">
+          <div className="project-row-desk">
             {projects.portfolio.name1}
             <br /> Framework: {projects.portfolio.framework1}
             <br /> Backend: {projects.portfolio.backend1}
             <br /> Hosted On: {projects.portfolio.hosted1}
           </div>
 
-          <div className="project-row-left">
+          <div className="project-row-desk">
             {projects.portfolio.name4}
             <br /> Framework: {projects.portfolio.framework4}
           </div>
-          <div className="project-row-left">
+          <div className="project-row-desk">
             {projects.portfolio.name5}
             <br /> Framework: {projects.portfolio.framework5}
           </div>
         </div>
         <div className="Row1">
-          <div className="project-row-left">
+          <div className="project-row-desk">
             {projects.portfolio.name2}
             <br /> Framework: {projects.portfolio.framework2}
             <br /> Backend: {projects.portfolio.backend2}
             <br /> Hosted On: {projects.portfolio.hosted2}
           </div>
-          <div className="project-row-left">
+          <div className="project-row-desk">
             {projects.portfolio.name3}
             <br /> Framework: {projects.portfolio.framework3}
             <br /> Backend: {projects.portfolio.backend3}
             <br /> Hosted On: {projects.portfolio.hosted3}
           </div>
-          <div className="project-row-left">
+          <div className="project-row-desk">
             Extras
             <br /> From Flatiron Bootcamp
           </div>
